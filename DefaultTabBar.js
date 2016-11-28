b0d1b3154ec3c50075d0e185821d676230358e73
Parminder Klair <perminder.klair@gmail.com>
2016-11-28 23:38:39 +0000

    fix

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -92,7 +92,7 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
   },
   tabs: {
-    height: 50,
+    height: 90,
     flexDirection: 'row',
     justifyContent: 'space-around',
     borderWidth: 1,
