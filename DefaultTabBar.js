e8383c553d21f3bcd93f10ed2974e718710369f6
Parminder Klair <perminder.klair@gmail.com>
2016-11-28 00:54:29 +0000

    crash fix

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactNative = require('react-native');
-const Icon = require('react-native-vector-icons/MaterialIcons');
+import Icon from 'react-native-vector-icons/MaterialIcons';
 const {
   StyleSheet,
   Text,
@@ -14,46 +14,46 @@ const DefaultTabBar = React.createClass({
     goToPage: React.PropTypes.func,
     activeTab: React.PropTypes.number,
     tabs: React.PropTypes.array,
-    underlineColor: React.PropTypes.string,
-    underlineHeight: React.PropTypes.number,
     backgroundColor: React.PropTypes.string,
     activeTextColor: React.PropTypes.string,
     inactiveTextColor: React.PropTypes.string,
     textStyle: Text.propTypes.style,
     tabStyle: View.propTypes.style,
+    renderTab: React.PropTypes.func,
+    underlineStyle: View.propTypes.style,
   },
 
   getDefaultProps() {
     return {
       activeTextColor: 'navy',
       inactiveTextColor: 'black',
-      underlineColor: 'navy',
       backgroundColor: null,
-      underlineHeight: 4,
     };
   },
 
   renderTabOption(name, page) {
-    const isTabActive = this.props.activeTab === page;
+  },
+
+  renderTab(name, page, isTabActive, onPressHandler) {
     const { activeTextColor, inactiveTextColor, textStyle, } = this.props;
     const textColor = isTabActive ? activeTextColor : inactiveTextColor;
     const fontWeight = isTabActive ? 'bold' : 'normal';
 
     return <Button
-      style={{flex: 1}}
+      style={{flex: 1, }}
       key={name}
       accessible={true}
       accessibilityLabel={name}
       accessibilityTraits='button'
-      onPress={() => this.props.goToPage(page)}
+      onPress={() => onPressHandler(page)}
     >
-      <View style={[styles.tab, this.props.tabStyle]}>
-          {isTabActive ? <Icon name="keyboard-arrow-up" size={34} color="c3cacd" /> : null}
-        <Text style={[{color: textColor, fontWeight, }, textStyle, ]}>
-            {name}
-        </Text>
-            {!isTabActive ? <Icon name="keyboard-arrow-down" size={34} color="c3cacd" /> : null}
-      </View>
+    <View style={[styles.tab, this.props.tabStyle]}>
+       {isTabActive ? <Icon name="keyboard-arrow-up" size={34} color="#c3cacd" /> : null}
+     <Text style={[{color: textColor, fontWeight, }, textStyle, ]}>
+         {name}
+     </Text>
+         {!isTabActive ? <Icon name="keyboard-arrow-down" size={34} color="#c3cacd" /> : null}
+    </View>
     </Button>;
   },
 
@@ -63,19 +63,22 @@ const DefaultTabBar = React.createClass({
     const tabUnderlineStyle = {
       position: 'absolute',
       width: containerWidth / numberOfTabs,
-      height: this.props.underlineHeight,
-      backgroundColor: this.props.underlineColor,
+      height: 4,
+      backgroundColor: 'navy',
       bottom: 0,
     };
 
     const left = this.props.scrollValue.interpolate({
       inputRange: [0, 1, ], outputRange: [0,  containerWidth / numberOfTabs, ],
     });
-
     return (
       <View style={[styles.tabs, {backgroundColor: this.props.backgroundColor, }, this.props.style, ]}>
-        {this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
-        <Animated.View style={[tabUnderlineStyle, { left, }, ]} />
+        {this.props.tabs.map((name, page) => {
+          const isTabActive = this.props.activeTab === page;
+          const renderTab = this.props.renderTab || this.renderTab;
+          return renderTab(name, page, isTabActive, this.props.goToPage);
+        })}
+        <Animated.View style={[tabUnderlineStyle, { left, }, this.props.underlineStyle, ]} />
       </View>
     );
   },
@@ -87,17 +90,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingBottom: 10,
-    paddingTop: 10
   },
   tabs: {
-    height: 90,
+    height: 50,
     flexDirection: 'row',
     justifyContent: 'space-around',
     borderWidth: 1,
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth: 0,
-    borderBottomColor: '#ccc',
+    borderColor: '#ccc',
   },
 });
 
